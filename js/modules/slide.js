70d0c679c9b3e2ef3ea484814bf3a4432d0105c5
lenrd <leonardo_aguiar@live.co.kr>
2020-09-29 14:58:46 -0300

    adicionado navegacao por botoes

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -1,6 +1,6 @@
 import debounce from "./debounce.js";
 
-export default class Slide {
+export class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
@@ -137,14 +137,29 @@ export default class Slide {
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
     this.onResize = debounce(this.onResize.bind(this), 150);
+    this.activeNextSlide = this.activeNextSlide.bind(this);
+    this.activePrevSlide = this.activePrevSlide.bind(this);
   }
 
   init() {
     this.slidesConfig();
     this.transition(true);
     this.bindEvents();
+    this.changeSlide(2);
     this.addSlideEvent();
     this.addResizeEvent();
     return this;
+  }
+}
+
+export class SlideNav extends Slide {
+  addArrow(prev, next) {
+    this.prevElement = document.querySelector(prev);
+    this.nextElement = document.querySelector(next);
+    this.addArrowEvent();
+  }
+  addArrowEvent() {
+    this.prevElement.addEventListener("click", this.activePrevSlide);
+    this.nextElement.addEventListener("click", this.activeNextSlide);
   }
 }
